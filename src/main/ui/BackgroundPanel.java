6a33d378c4032e5d558936b045ac3d4aec460198
swang139 <shengnaaaa@gmail.com>
2023-03-30 22:08:44 -0700

    merge messed up with my commit

--- src/main/ui/BackgroundPanel.java
+++ src/main/ui/BackgroundPanel.java
@@ -7,12 +7,12 @@ public class BackgroundPanel extends JPanel {
     Image background = new ImageIcon("data/leather_background.png").getImage();
 
     public BackgroundPanel() {
-        setPreferredSize(new Dimension(600, 500));
+        setPreferredSize(new Dimension(800, 800));
     }
 
     public void paint(Graphics g) {
         super.paint(g);
         Graphics2D g2D = (Graphics2D) g;
-        g2D.drawImage(background, 0, 0, 600, 500, null);
+        g2D.drawImage(background, 100, 0, 700, 700, null);
     }
 }
